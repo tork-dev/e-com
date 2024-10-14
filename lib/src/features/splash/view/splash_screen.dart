620d6f190f8936a4be2cbf40fabd1052726becbd
Flutter Intern <flutter1.tork@gmail.com>
2024-10-14 17:47:34 +0600

    added a floating img for beta sign

--- lib/src/features/splash/view/splash_screen.dart
+++ lib/src/features/splash/view/splash_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import '../../../utils/constants/colors.dart';
 import '../../../utils/constants/image_strings.dart';
 import '../../../utils/helpers/helper_functions.dart';
 import '../controller/splash_controller.dart';
-
-
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -14,7 +13,9 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.put(SplashController());
     final isDark = AppHelperFunctions.isDarkMode(context);
+    final baseUrl = dotenv.env['BASE_URL'];
     return Scaffold(
+
       backgroundColor: isDark ? AppColors.dark : AppColors.primaryBackground,
       body: Center(
         child: Image(
@@ -22,6 +23,15 @@ class SplashScreen extends StatelessWidget {
           image: const AssetImage(AppImages.lightAppLogo),
         ),
       ),
+      floatingActionButton: Visibility(
+        visible: baseUrl!.contains('beta'),
+        child: Image(
+          width: AppHelperFunctions.screenWidth() * .3,
+          fit: BoxFit.cover,
+          image: const AssetImage(AppImages.betaLogo),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniStartTop,
     );
   }
 }
