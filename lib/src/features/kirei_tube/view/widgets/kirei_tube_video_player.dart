b3e1a79b7d7ca1e0886e83d0b8f0732128db4d8a
Flutter Intern <flutter1.tork@gmail.com>
2024-10-20 18:16:13 +0600

    remove some code

--- lib/src/features/kirei_tube/view/widgets/kirei_tube_video_player.dart
+++ lib/src/features/kirei_tube/view/widgets/kirei_tube_video_player.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:kirei/src/common/styles/skeleton_style.dart';
 import 'package:kirei/src/common/widgets/containers/card_container.dart';
-import 'package:kirei/src/features/web_view/web_view.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 import '../../controller/kirei_tube_details_controller.dart';
@@ -17,12 +15,7 @@ class KireiDetailsVideoPlayer extends StatelessWidget {
     return AppCardContainer(
         height: 250,
         child: Obx(() {
-
-          return controller.hittingApi.value? ShimmerHelper().buildBasicShimmer(height: 250) :
-            controller.kireiTubeDetailsResponse.value.data!.video!.contains('www.facebook.com')?
-
-
-            controller.youtubeController.value == null
+          return controller.youtubeController.value == null
               ? ShimmerHelper().buildBasicShimmer()
               : SizedBox(
             height: 250,
