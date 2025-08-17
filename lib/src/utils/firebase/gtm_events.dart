8c0c844fc3f202ee93693de3b130dfea237c0f93
Flutter Intern <flutter1.tork@gmail.com>
2025-08-17 13:04:42 +0600

    remove gtm from the project

--- lib/src/utils/firebase/gtm_events.dart
+++ lib/src/utils/firebase/gtm_events.dart
@@ -1,6 +1,6 @@
 import 'package:facebook_app_events/facebook_app_events.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:gtm/gtm.dart';
+// import 'package:gtm/gtm.dart';
 import 'package:kirei/src/utils/local_storage/local_storage_keys.dart';
 import 'package:kirei/src/utils/local_storage/storage_utility.dart';
 import 'package:kirei/src/utils/logging/logger.dart';
@@ -8,7 +8,7 @@ import 'package:kirei/src/utils/logging/logger.dart';
 class EventLogger {
   final FirebaseAnalytics _analytics = FirebaseAnalytics.instance;
   FacebookAppEvents facebookAppEvents = FacebookAppEvents();
-  Gtm gtm = Gtm.instance;
+  // Gtm gtm = Gtm.instance;
 
   final String? memberId =
       AppLocalStorage().readData(LocalStorageKeys.userEmail).toString();
@@ -27,11 +27,11 @@ class EventLogger {
     //   'item_id': itemId,
     // });
 
-    gtm.push('product_details_view', parameters: {
-      'member_id': memberId,
-      'item_id': itemId,
-      'value' : '$price'
-    });
+    // gtm.push('product_details_view', parameters: {
+    //   'member_id': memberId,
+    //   'item_id': itemId,
+    //   'value' : '$price'
+    // });
 
     facebookAppEvents.logEvent(
       name: 'ViewContent',
@@ -57,13 +57,13 @@ class EventLogger {
     // });
 
     // Google Tag Manager
-    gtm.push('purchase', parameters: {
-      'transaction_id': orderId,
-      'value': itemPrice.toDouble(),
-      'currency': 'BDT',
-      'member_id': memberId,
-      'items': items,
-    });
+    // gtm.push('purchase', parameters: {
+    //   'transaction_id': orderId,
+    //   'value': itemPrice.toDouble(),
+    //   'currency': 'BDT',
+    //   'member_id': memberId,
+    //   'items': items,
+    // });
 
     // Facebook App Events
     facebookAppEvents.logEvent(
@@ -82,12 +82,12 @@ class EventLogger {
 
   void logAddToWishlistEvent(String itemId, dynamic itemPrice) async {
 
-    gtm.push('add_to_wishlist', parameters: {
-      'member_id': memberId,
-      'item_id': itemId,
-      'value': itemPrice.toDouble(),
-      'currency': 'BDT',
-    });
+    // gtm.push('add_to_wishlist', parameters: {
+    //   'member_id': memberId,
+    //   'item_id': itemId,
+    //   'value': itemPrice.toDouble(),
+    //   'currency': 'BDT',
+    // });
 
     facebookAppEvents.logEvent(
       name: 'AddToWishlist',
@@ -112,12 +112,12 @@ class EventLogger {
     //   'currency': 'BDT',
     // });
 
-    gtm.push('add_to_cart', parameters: {
-      'member_id': memberId,
-      'item_id': itemId,
-      'value': itemPrice.toDouble(),
-      'currency': 'BDT',
-    });
+    // gtm.push('add_to_cart', parameters: {
+    //   'member_id': memberId,
+    //   'item_id': itemId,
+    //   'value': itemPrice.toDouble(),
+    //   'currency': 'BDT',
+    // });
 
     facebookAppEvents.logEvent(
       name: 'AddToCart',
@@ -145,10 +145,10 @@ class EventLogger {
     //   'member_id': memberId!
     // });
 
-    gtm.push('search', parameters: {
-      'search_value': searchValue,
-      'member_id': memberId
-    });
+    // gtm.push('search', parameters: {
+    //   'search_value': searchValue,
+    //   'member_id': memberId
+    // });
 
     facebookAppEvents.logEvent(
       name: 'Search',
@@ -169,12 +169,12 @@ class EventLogger {
     //   'currency': 'BDT',
     // });
 
-    gtm.push('initial_checkout', parameters: {
-      'cart_id': cartId,
-      'member_id': memberId,
-      'value': double.parse(subtotalPrice.toString()),
-      'currency': 'BDT',
-    });
+    // gtm.push('initial_checkout', parameters: {
+    //   'cart_id': cartId,
+    //   'member_id': memberId,
+    //   'value': double.parse(subtotalPrice.toString()),
+    //   'currency': 'BDT',
+    // });
 
     facebookAppEvents.logEvent(
       name: 'InitiateCheckout',
@@ -202,9 +202,9 @@ class EventLogger {
     //   "method": loginMethod
     // });
 
-    gtm.push('login', parameters: {
-      "method": loginMethod
-    });
+    // gtm.push('login', parameters: {
+    //   "method": loginMethod
+    // });
 
     facebookAppEvents.logEvent(
       name: 'Login',
@@ -220,9 +220,9 @@ class EventLogger {
     //   "method": loginMethod
     // });
 
-    gtm.push('login', parameters: {
-      "method": loginMethod
-    });
+    // gtm.push('login', parameters: {
+    //   "method": loginMethod
+    // });
 
     facebookAppEvents.logEvent(
       name: 'login',
@@ -239,10 +239,10 @@ class EventLogger {
     //   "item_id": itemId,
     // });
 
-    gtm.push('share', parameters: {
-      "content_type": "product",
-      "item_id": itemId,
-    });
+    // gtm.push('share', parameters: {
+    //   "content_type": "product",
+    //   "item_id": itemId,
+    // });
 
     facebookAppEvents.logEvent(
       name: 'Share',
@@ -264,12 +264,12 @@ class EventLogger {
     //   'currency': 'BDT',
     // });
 
-    gtm.push('remove_from_cart', parameters: {
-      'member_id': memberId,
-      'item_id': itemId,
-      'value': itemPrice.toDouble(),
-      'currency': 'BDT',
-    });
+    // gtm.push('remove_from_cart', parameters: {
+    //   'member_id': memberId,
+    //   'item_id': itemId,
+    //   'value': itemPrice.toDouble(),
+    //   'currency': 'BDT',
+    // });
 
     facebookAppEvents.logEvent(
       name: 'RemoveFromCart',
@@ -294,12 +294,12 @@ class EventLogger {
     //   'currency': 'BDT'
     // });
 
-    gtm.push('view_cart', parameters: {
-      'member_id': memberId,
-      'items': items,
-      'value': totalValue.toDouble(),
-      'currency': 'BDT'
-    });
+    // gtm.push('view_cart', parameters: {
+    //   'member_id': memberId,
+    //   'items': items,
+    //   'value': totalValue.toDouble(),
+    //   'currency': 'BDT'
+    // });
 
     facebookAppEvents.logEvent(
       name: 'ViewCart',
