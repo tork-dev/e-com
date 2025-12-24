97977d39b5621988e32e44c87d895c054c0ca980
Flutter Intern <flutter1.tork@gmail.com>
2025-12-24 17:39:42 +0600

    New Setup added for ios migration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,42 +1,36 @@
 import UIKit
 import Flutter
 import Firebase
-import flutter_local_notifications
 import FBSDKCoreKit
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        // Firebase initialization
         FirebaseApp.configure()
 
-        // Facebook SDK initialization
         ApplicationDelegate.shared.application(
             application,
             didFinishLaunchingWithOptions: launchOptions
         )
 
-        // Setup for flutter_local_notifications
-        FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-            GeneratedPluginRegistrant.register(with: registry)
-        }
-
-        // Delegate for notifications (iOS 10+)
         if #available(iOS 10.0, *) {
-            UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+            UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
         }
 
-        // Register plugins
         GeneratedPluginRegistrant.register(with: self)
-
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
-    // Facebook URL handling for deep linking
+    // This handles plugin registration for the new Scene Lifecycle
+    @objc func didInitializeImplicitFlutterEngine(_ engine: FlutterEngine) {
+        GeneratedPluginRegistrant.register(with: engine)
+    }
+
     override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        return ApplicationDelegate.shared.application(app, open: url, options: options) || super.application(app, open: url, options: options)
+        return ApplicationDelegate.shared.application(app, open: url, options: options)
+               || super.application(app, open: url, options: options)
     }
 }
---
+++ ios/Runner/SceneDelegate.swift
@@ -0,0 +1,6 @@
+import Flutter
+import UIKit
+
+class SceneDelegate: FlutterSceneDelegate {
+    // Standard implementation for Flutter
+}
